edb48e3a6271494b09ad8d05222f2b807d2daeed
Vasco Magolo <vascomagolo@gmail.com>
2024-11-05 13:34:17 +0000

    updated website

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@ addEventListener('load', function() {
     const currentpage = window.location.pathname;
     const nav_cv = document.getElementById('nav_cv');
     const nav_portfolio = document.getElementById('nav_portfolio');
+    const nav_about = document.getElementById('nav_about');
+    const nav_project = document.getElementById('nav_project');
 
     if (currentpage.includes('cv.html')) {
-        nav_cv.classList.add('active');
-        nav_portfolio.classList.remove('active');
+        nav_cv.classList.add('deactive');
+        nav_about.classList.add('deactive');
+        nav_project.classList.add('deactive');
+        nav_portfolio.classList.remove('deactive');
     }
     else {
-        nav_cv.classList.remove('active');
-        nav_portfolio.classList.add('active');
+        nav_cv.classList.remove('deactive');
+        nav_portfolio.classList.add('deactive');
+        nav_about.classList.remove('deactive');
+        nav_project.classList.remove('deactive');
     }
 });
 
